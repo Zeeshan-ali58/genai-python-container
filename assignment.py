0c9f5abadbb50790a050ed84278a8df410c72dec
Zeeshan Ali <zeeshanali.4611@gmail.com>
2024-08-22 18:38:01 +0000

    assignment for 25 Aug 24

--- assignment.py
+++ assignment.py
@@ -1,21 +1,73 @@
 from datetime import datetime,timedelta
-
+import math
+#calculating age on years month and days
 dob = datetime(1992, 6, 29)
 current_date = datetime.now()
 
-age_years = current_date.year - dob.year
-age_months = current_date.month - dob.month
-age_days = current_date.day - dob.day
+dobYears = current_date.year - dob.year 
+dobMonths =current_date.month - dob.month
+dobDays =current_date.day - dob.day
 
-# Adjust for cases where the current month or day is before the birth month or day
-if age_days < 0:
-    age_months -= 1
-    days_in_prev_month = (current_date.replace(day=1) - timedelta(days=1)).day
-    age_days += days_in_prev_month
+if dobDays < 0:
+    dobDays -=1
+    daysInPrevMonth = (current_date.replace(day=1) - timedelta(days=1)).day
+    dobDays += daysInPrevMonth
 
-if age_months < 0:
-    age_years -= 1
-    age_months += 12
+if dobMonths < 0:
+    dobYears -=1
+    dobMonths += 12
 
-# Print the result
-print(f"You are {age_years} years, {age_months} months, and {age_days} days old.")
+print(f"You are {dobYears} years, {dobMonths} months, and {dobDays} days old.")
+
+# now we'll be calculating the area of rectangle
+recLength = 15
+recWidth = 10
+recArea = recLength * recWidth
+print(f"The areas of rectagle us {recArea} unit squares")
+
+# now we'll be calculating the area of circle
+diameter = 100
+radius = 100/2
+circleArea = math.pi * (radius**2)
+print(f"Area of circle is {circleArea:.2f}")
+
+# now we'll be calculating the area of cube
+cubeSides = 6 # a cube has six sides
+sideArea = 10
+cubeArea = 6 * (sideArea**2)
+print(f"Area of cube is {cubeArea}")
+
+# now we'll be calculating the volume of cylinder
+cycilderRadius = 5 # a cube has six sides
+cylinderHeight = 20
+cylinderVolume = math.pi * (cycilderRadius**2) * cylinderHeight
+print(f"Volume of cylinder is {cylinderVolume:.2f}")
+
+#calculating BMI (Body mass index). Formuls is squre of weight/heighnt in meters
+heightInMeters = 5 * 0.3048 # 0.3048 unit in meters against 1 foot
+heightInInches = 7 * 0.0254
+height =heightInMeters + heightInInches
+weight = 84
+bmiCalculation = weight/ height**2
+print(f"My BMI according to height and weight is {bmiCalculation:.2f}")
+
+
+# now we'll be converting seconds into minutes
+totalSeconds = 3600 
+secondsInOneMinute = 60
+calculateInMinutes = int(totalSeconds/secondsInOneMinute)
+print(f"There are {calculateInMinutes} minutes according of {totalSeconds} seconds" )
+
+
+# now we'll be converting temperature from fahrenheit to celcius and vise versa
+tempInCelcius = 48 
+tempInFahrenheit = tempInCelcius * (9/5) + 32
+tempInCelciusReConvert = (tempInFahrenheit -32) * 5/9
+print(f"Temperature in Fahrenheit is {tempInFahrenheit} and by converting Fahrenheit into Celcius the result is {tempInCelciusReConvert}" )
+
+
+#calculating percentage
+obtainedMarks = 840
+totalMarks = 1050
+calculatePercentage = (obtainedMarks/totalMarks) * 100
+print(f"Total obtained marrks percentage is {calculatePercentage:.2f} percentage" )
